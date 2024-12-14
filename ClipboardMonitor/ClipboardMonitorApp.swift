b9f4911938e3b59c6f83495fb6aae80ebd1ff5a4
Emielex <emiel.deboyser@student.arteveldehs.be>
2024-12-14 16:19:35 +0100

    Initial Commit

---
+++ ClipboardMonitor/ClipboardMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardMonitorApp.swift
+//  ClipboardMonitor
+//
+//  Created by Emiel Deboyser on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
